5a1bb9f6ab93c12dfed741bbbfab211ddeb5fbb6
SaraNejmanowska <38699183+SaraNejmanowska@users.noreply.github.com>
2021-01-19 12:17:19 +0100

    Add files via upload

---
+++ e-stolowka/scripts.js
@@ -0,0 +1,8 @@
+  function openNav() {
+    var x = document.getElementById("mySidenav");
+    if (x.style.width == "70px") {
+      x.style.width = "169px";
+    } else {
+      x.style.width = "70px";
+    }
+  }
