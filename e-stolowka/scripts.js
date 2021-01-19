c005638163c0bdccef86ff541d92e57ae90934ad
SaraNejmanowska <38699183+SaraNejmanowska@users.noreply.github.com>
2021-01-19 12:14:38 +0100

    Delete e-stolowka directory

--- e-stolowka/scripts.js
+++
@@ -1,8 +0,0 @@
-  function openNav() {
-    var x = document.getElementById("mySidenav");
-    if (x.style.width == "70px") {
-      x.style.width = "169px";
-    } else {
-      x.style.width = "70px";
-    }
-  }
